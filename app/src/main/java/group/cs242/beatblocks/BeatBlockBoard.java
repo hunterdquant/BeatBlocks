8a97fcd7280c84063cec5e24ce8acfd01eb8772b
Hunter Quant <hunterdquant@gmail.com>
2015-10-25 18:01:41 -0400

    Implemented checkMatches in BeatBlockBoard.

--- app/src/main/java/group/cs242/beatblocks/BeatBlockBoard.java
+++ app/src/main/java/group/cs242/beatblocks/BeatBlockBoard.java
@@ -1,24 +1,16 @@
 package group.cs242.beatblocks;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
- * Created by hunte on 10/25/2015.
+ * Created by Hunter on 10/25/2015.
  */
 public class BeatBlockBoard extends GameBoard{
 
-    private enum Block {
-        RED(1), BLUE(2), GREEN(3), YELLOW(4);
-
-        private int value;
-
-        private Block(int value) {
-            this.value = value;
-        }
-    }
-
     public BeatBlockBoard() {
-        board = new int[7][7];
+        board = new byte[7][7];
     }
 
     protected void populate() {
@@ -28,13 +20,63 @@ public class BeatBlockBoard extends GameBoard{
                 for (int j = 0; j < board[0].length; j++) {
                     if (board[i][j] != 0) continue;
                     //Adding 1 to exclude zero.
-                    int blockType = rand.nextInt(4) + 1;
+                    byte blockType = (byte)(rand.nextInt(4) + 1);
                     board[i][j] = blockType;
                 }
             }
         } catch (Exception e){
             System.err.println("Unexpected error");
             e.printStackTrace();
+        }
+        checkMatches();
+    }
+
+    protected void checkMatches() {
+        List<Index> matchedIndices = new ArrayList<Index>();
+        try {
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[0].length; j++) {
+                    if (i > 1) {
+                        if (board[i-2][j] == board[i][j] && board[i-1][j] == board[i][j]) {
+                            matchedIndices.add(new Index(i-2, j));
+                            matchedIndices.add(new Index(i-1, j));
+                            matchedIndices.add(new Index(i, j));
+                        }
+                    }
+                    if (j > 1) {
+                        if (board[i][j-2] == board[i][j] && board[i][j-1] == board[i][j]) {
+                            matchedIndices.add(new Index(i, j-2));
+                            matchedIndices.add(new Index(i, j-1));
+                            matchedIndices.add(new Index(i, j));
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Unexpected error");
+            e.printStackTrace();
+        }
+    }
+
+    protected void removeMatches(List<Index> matchedIndices) {
+
+    }
+
+    class Index {
+        private int i;
+        private int j;
+
+        private Index(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+
+        private int getI() {
+            return i;
+        }
+
+        private int getJ() {
+            return j;
         }
     }
 }
--- app/src/main/java/group/cs242/beatblocks/GameBoard.java
+++ app/src/main/java/group/cs242/beatblocks/GameBoard.java
@@ -2,15 +2,16 @@ package group.cs242.beatblocks;
 
 import android.util.Log;
 
+import java.util.List;
 import java.util.Random;
 /**
  * Created by Hunter on 10/25/2015.
  */
 abstract class GameBoard {
 
-    protected int [][] board;
+    protected byte [][] board;
 
     protected abstract void populate();
-    //protected abstract void checkMatches();
-    //protected abstract void removeMatches();
+    protected abstract void checkMatches();
+    protected abstract void removeMatches(List<BeatBlockBoard.Index> matchedIndices);
 }
