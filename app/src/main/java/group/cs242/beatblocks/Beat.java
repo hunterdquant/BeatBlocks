8eadd6c3b0abb57e2a9ef66ce92a8217bc9497e9
Cameron Icso <icsoc@clarkson.edu>
2015-11-16 14:51:35 -0500

    Overhaul of Beat and BeatMap, added BeatMapView

--- app/src/main/java/group/cs242/beatblocks/Beat.java
+++ app/src/main/java/group/cs242/beatblocks/Beat.java
@@ -1,50 +1,56 @@
 package group.cs242.beatblocks;
 
+import android.animation.ValueAnimator;
+import android.graphics.Rect;
+
 /**
  * @author Cameron Icso <icsoc@clarkson.edu> <icsoc22@gmail.com>
  *
  *  Represents an idividual Beat to be used in the BeatMap
  */
-public class Beat {
+public class Beat implements ValueAnimator.AnimatorUpdateListener{
     //Data Members
-    int rightbound;  //The right bound of the beat
-    int leftbound;   //The left bound of the beat
+    Rect rectangle;
     boolean used;    //represents if the beat has been used yet
 
-
+    int cent;
+    ValueAnimator centAnim;
 
     //Constructors
-    Beat(int right, int left)
+    Beat(int left, int top, int right, int bottom, long time_delay, long mspb)
     {
-        rightbound = right;
-        leftbound = left;
+        rectangle = new Rect(left, top, right, bottom);
         used = false;
+        cent = (right+left)/2;
+        centAnim = new ValueAnimator();
+        centAnim.setIntValues(cent, 1100);
+        centAnim.setInterpolator(null);
+        centAnim.setDuration(10*mspb);
+        centAnim.setStartDelay(time_delay);
+        centAnim.addUpdateListener(this);
     }
 
     //Methods
-    int getLeft()   //Returns the Leftbound member
-    {
-        return  leftbound;
-    }
-
-    int getRight()   //Returns the Rightbound member
-    {
-        return  rightbound;
-    }
-
-    boolean getUsed()   //Returns if the beat has been used or not
+    public boolean getUsed()   //Returns if the beat has been used or not
     {
         return used;
     }
 
-    void setUsed(boolean u)
+    public void setUsed(boolean u)   //Sets used to the given value
     {
         used = u;
     }
 
-    void draw()
+    @Override public void onAnimationUpdate(ValueAnimator animation)
     {
-        //How the Beat will be displayed
-        //Animation thingy
+        rectangle.right = ((Integer) centAnim.getAnimatedValue()) + 20;
+        rectangle.left = ((Integer) centAnim.getAnimatedValue()) - 20;
+
     }
+
+    void Animate()
+    {
+        centAnim.start();
+    }
+
 }
