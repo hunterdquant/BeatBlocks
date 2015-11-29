2fa1a6cdab1f0f4d730496dd1b37153402b2b654
Hunter Quant <hunterdquant@gmail.com>
2015-11-29 15:17:57 -0500

    Added UML class diagram of the project using the SimpleUMLCE plugin.

--- app/src/main/java/group/cs242/beatblocks/StartScreenActivity.java
+++ app/src/main/java/group/cs242/beatblocks/StartScreenActivity.java
@@ -4,12 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
-
-import java.util.zip.Inflater;
 
 public class StartScreenActivity extends AppCompatActivity {
 
